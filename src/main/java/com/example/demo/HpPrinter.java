f2ed8ef1c01b4aeb941267d58b78bc9f896d4ccf
limeng <limeng.chen@flowring.com>
2022-01-06 22:30:57 +0800

    init: 2-4 @Configuration

---
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+public class HpPrinter implements Printer {
+    @Override
+    public void print(String message) {
+        System.out.println("HP 印表機" + message);
+    }
+}
---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    private  Printer printer;
+
+    @RequestMapping("/test")
+    public String test() {
+        printer.print("Hello world");
+        return "Hello world";
+    }
+}
