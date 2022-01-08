bdd00e475f5e9af198d9a1e6568f43d8b4a6fbd4
limeng <limeng.chen@flowring.com>
2022-01-08 21:55:47 +0800

    feat:2-6設定檔

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -7,16 +8,17 @@ import javax.annotation.PostConstruct;
 @Component
 public class HpPrinter implements Printer {
 
+    @Value("${printer.name}")
+    private String name;
+
+    @Value("${printer.count:20}")
     private int count;
-    @PostConstruct
-    public void init() {
-        count = 5;
-    }
+
 
     @Override
     public void print(String message) {
         count--;
-        System.out.println("HP 印表機" + message);
+        System.out.println(name + ":" + message);
         System.out.println("剩餘使用次數:" + count);
     }
 }
