5a5f6f65e27708fa297e9f129762c4220716a26c
limeng <limeng.chen@flowring.com>
2022-01-09 21:50:54 +0800

    4-3URL路徑對應

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/detail")
 @RestController
 public class MyController {
+    @RequestMapping("/product")
+    public String product() {
+        return "第一個是蘋果，第二個是橘子";
+    }
 
-    @Autowired
-    private  Printer printer;
-
-    @RequestMapping("/test")
-    public String test() {
-        printer.print("Hello world");
-        return "Hello world";
+    @RequestMapping("/user")
+    public String user() {
+        return "名字為 Judy";
     }
 }
+
