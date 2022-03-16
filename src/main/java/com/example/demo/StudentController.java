aa6d91e8bcaaf8c5c9ebf658b34250a1e0d6443a
limeng <limeng.chen@flowring.com>
2022-03-16 22:17:15 +0800

    6-4. 使用 CrudRepository 執行資料庫的 CRUD 操作

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -15,5 +13,31 @@ public class StudentController {
     public String insert(@RequestBody Student student) {
         studentRepository.save(student);
         return "執行 create 操作";
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String update(@PathVariable Integer studentId,
+                         @RequestBody Student student) {
+        Student s = studentRepository.findById(studentId).orElse(null);
+        if (s != null) {
+            s.setName(student.getName());
+            studentRepository.save(s);
+            return "執行 update 操作";
+        } else {
+            return "update 失敗，數據不存在";
+        }
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Integer studentId) {
+        studentRepository.deleteById(studentId);
+        return "執行 delete 操作";
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student read(@PathVariable Integer studentId) {
+        // orElse 若在資料庫找不到此值的話，則回傳 null
+        Student student = studentRepository.findById(studentId).orElse(null);
+        return student;
     }
 }
